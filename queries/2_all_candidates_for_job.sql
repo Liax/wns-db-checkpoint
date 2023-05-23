b3af486aa828744082d6027b8c8db3cfcfc398dd
Clara Martin <clara@studiometa.fr>
2023-05-23 16:04:24 +0200

    Get all candidates for job

--- queries/2_all_candidates_for_job.sql
+++ queries/2_all_candidates_for_job.sql
@@ -1,2 +1,7 @@
 # Écris une requêtes SQL qui retourne tous les candidats ayant répondu à l'offre
 # intitulée "Dev"
+SELECT *
+FROM CANDIDATE
+JOIN CANDIDATURE ON CANDIDATURE.candidate_id = CANDIDATE.id
+JOIN OFFER ON  OFFER.id = CANDIDATURE.jobOffer_id
+WHERE OFFER.title = "Dev";

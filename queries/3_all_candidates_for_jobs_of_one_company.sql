7fce1a30321a0b8e9caf6d1e747ac011d4af4d53
Clara Martin <clara@studiometa.fr>
2023-05-23 16:14:28 +0200

    All candidates for jobs of one company

--- queries/3_all_candidates_for_jobs_of_one_company.sql
+++ queries/3_all_candidates_for_jobs_of_one_company.sql
@@ -1,2 +1,8 @@
 # Écris une requêtes SQL qui retourne tous les candidats ayant répondu à 
 # au moins une offre d'une entreprise identifiée par son nom
+SELECT DISTINCT *
+FROM Candidate 
+JOIN Candidature ON Candidate.id = Candidature.candidate_id
+JOIN Offer ON Candidature.jobOffer_id = Offer.id
+JOIN Enterprise ON Offer.enterprise_id = Enterprise.id
+WHERE Enterprise.name = 'WCS';
